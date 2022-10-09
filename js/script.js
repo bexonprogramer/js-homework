6e4a6af72f68db1efff44b321b172a9d60b0ec50
bexroz <bexrozsamadov20@gmail.com>
2022-10-09 20:40:22 +0300

    my (if else)

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// task 1
+
+let a=prompt('son kiriting','');
+
+if(a%2==0 && a>0 ){
+    alert('juft')
+}else if(!a%2==0 && a>0 ){
+    alert('toq')
+}else confirm('faqat son kiritilsin');
+
+
+// task 2
+
+let parol1=prompt('16 raqamli parolni kiriting !!','0000000000000000');
+
+if(parol1.length==16){
+    alert(parol1)
+}else alert('xatolik');
